08ff32985da97d38872944da99c94f2f410ddbfb
Anatoliy Dudar <gigsollt@gmail.com>
2025-04-28 00:24:27 +0300

    added custom exceptions

---
+++ classes/exceptions.py
@@ -0,0 +1,16 @@
+class NotARectangle(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class NotASquare(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
