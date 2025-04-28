33be7d85d2519a394954b72b126612551f8b2af8
Anatoliy Dudar <gigsollt@gmail.com>
2025-04-28 13:27:20 +0300

    added a class for square

---
+++ classes/square.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from rhombus import Rhombus
+from exceptions import NotASquare
+
+
+@dataclass
+class Square(Rhombus):
+    def __post_init__(self):
+        diag_lengths = self.calculate_diagonals()
+        if diag_lengths[0] != diag_lengths[1]:
+            raise NotASquare("Diagonals have different length,"
+                             "so it isn't a Square")
