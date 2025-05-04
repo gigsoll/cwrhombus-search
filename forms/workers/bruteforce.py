d58deac9fa0ec28685c1ef7da523d74084ddbeec
Anatoliy Dudar <gigsollt@gmail.com>
2025-05-04 10:03:25 +0300

    create QtWorker

---
+++ forms/workers/bruteforce.py
@@ -0,0 +1,22 @@
+from PyQt6.QtCore import QObject, pyqtSignal
+from solutions.bruteforce import brutforce
+
+
+class Worker(QObject):
+    rhomb_progress = pyqtSignal(int, int)
+    square_progress = pyqtSignal(int, int)
+    finished = pyqtSignal(object, object)
+
+    def __init__(self, file):
+        super().__init__()
+        self.file = file
+
+    def run(self):
+        def on_rhomb(i, total):
+            self.rhomb_progress.emit(i, total)
+
+        def on_square(i, total):
+            self.square_progress.emit(i, total)
+
+        squares, rhombs = brutforce(self.file, on_rhomb, on_square)
+        self.finished.emit(squares, rhombs)
