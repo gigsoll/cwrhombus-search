28d1340ffa82ac00bbdfc5d99b72d7864ba7b763
Anatoliy Dydar <72531963+gigsoll@users.noreply.github.com>
2025-05-14 02:48:04 +0300

    typo fix

--- forms/workers/graph.py
+++ forms/workers/graph.py
@@ -1,4 +1,4 @@
-gimport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from classes.point import point_reader, Point
 from classes.rhombus import Rhombus
 from classes.square import Square
